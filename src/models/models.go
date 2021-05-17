bc009cfd69e6bcedb0cc56bd5a3cfa4ece685069
kamaal111 <kamaal.f1@gmail.com>
2021-05-17 21:23:14 +0200

    Only exporting fields that are needed to be exported

--- src/models/models.go
+++ src/models/models.go
@@ -19,7 +19,7 @@ type MetricsTable struct {
 	ID              int               `pg:"id,pk"`
 	AppVersion      string            `pg:"app_version"`
 	AppBuildVersion string            `pg:"app_build_version"`
-	Payload         CollectionMetrics `pg:"payload"`
+	Payload         collectionMetrics `pg:"payload"`
 	AppID           int               `pg:"app_id"`
 	App             *AppsTable        `pg:"app,rel:has-one"`
 }
@@ -32,29 +32,29 @@ func (metric *MetricsTable) Save(pgDB *pg.DB) error {
 type CollectionPayload struct {
 	BundleIdentifier string            `json:"bundle_identifier,omitempty"`
 	AppVersion       string            `json:"app_version,omitempty"`
-	Payload          CollectionMetrics `json:"payload"`
+	Payload          collectionMetrics `json:"payload"`
 }
 
-type CollectionMetrics struct {
-	LocationActivityMetrics          LocationActivityMetrics          `json:"locationActivityMetrics,omitempty"`
-	CellularConditionMetrics         CellularConditionMetrics         `json:"cellularConditionMetrics,omitempty"`
+type collectionMetrics struct {
+	LocationActivityMetrics          locationActivityMetrics          `json:"locationActivityMetrics,omitempty"`
+	CellularConditionMetrics         cellularConditionMetrics         `json:"cellularConditionMetrics,omitempty"`
 	MetaData                         MetaData                         `json:"metaData,omitempty"`
-	GPUMetrics                       GPUMetrics                       `json:"gpuMetrics,omitempty"`
-	MemoryMetrics                    MemoryMetrics                    `json:"memoryMetrics,omitempty"`
-	SignpostMetrics                  []SignpostMetrics                `json:"signpostMetrics,omitempty"`
-	DisplayMetrics                   DisplayMetrics                   `json:"displayMetrics,omitempty"`
-	CPUMetrics                       CPUMetrics                       `json:"cpuMetrics,omitempty"`
-	NetworkTransferMetrics           NetworkTransferMetrics           `json:"networkTransferMetrics,omitempty"`
-	DiskIOMetrics                    DiskIOMetrics                    `json:"diskIOMetrics,omitempty"`
-	ApplicationLaunchMetrics         ApplicationLaunchMetrics         `json:"applicationLaunchMetrics,omitempty"`
-	ApplicationTimeMetrics           ApplicationTimeMetrics           `json:"applicationTimeMetrics,omitempty"`
+	GPUMetrics                       gpuMetrics                       `json:"gpuMetrics,omitempty"`
+	MemoryMetrics                    memoryMetrics                    `json:"memoryMetrics,omitempty"`
+	SignpostMetrics                  []signpostMetrics                `json:"signpostMetrics,omitempty"`
+	DisplayMetrics                   displayMetrics                   `json:"displayMetrics,omitempty"`
+	CPUMetrics                       cpuMetrics                       `json:"cpuMetrics,omitempty"`
+	NetworkTransferMetrics           networkTransferMetrics           `json:"networkTransferMetrics,omitempty"`
+	DiskIOMetrics                    diskIOMetrics                    `json:"diskIOMetrics,omitempty"`
+	ApplicationLaunchMetrics         applicationLaunchMetrics         `json:"applicationLaunchMetrics,omitempty"`
+	ApplicationTimeMetrics           applicationTimeMetrics           `json:"applicationTimeMetrics,omitempty"`
 	TimeStampEnd                     string                           `json:"timeStampEnd,omitempty"`
-	ApplicationResponsivenessMetrics ApplicationResponsivenessMetrics `json:"applicationResponsivenessMetrics,omitempty"`
+	ApplicationResponsivenessMetrics applicationResponsivenessMetrics `json:"applicationResponsivenessMetrics,omitempty"`
 	AppVersion                       string                           `json:"appVersion,omitempty"`
 	TimeStampBegin                   string                           `json:"timeStampBegin,omitempty"`
 }
 
-type LocationActivityMetrics struct {
+type locationActivityMetrics struct {
 	CumulativeBestAccuracyForNavigationTime string `json:"cumulativeBestAccuracyForNavigationTime,omitempty"`
 	CumulativeBestAccuracyTime              string `json:"cumulativeBestAccuracyTime,omitempty"`
 	CumulativeHundredMetersAccuracyTime     string `json:"cumulativeHundredMetersAccuracyTime,omitempty"`
@@ -63,16 +63,16 @@ type LocationActivityMetrics struct {
 	CumulativeThreeKilometersAccuracyTime   string `json:"cumulativeThreeKilometersAccuracyTime,omitempty"`
 }
 
-type CellularConditionMetrics struct {
-	CellConditionTime CellConditionTime `json:"cellConditionTime,omitempty"`
+type cellularConditionMetrics struct {
+	CellConditionTime cellConditionTime `json:"cellConditionTime,omitempty"`
 }
 
-type CellConditionTime struct {
+type cellConditionTime struct {
 	HistogramNumBuckets int64                     `json:"histogramNumBuckets,omitempty"`
-	HistogramValue      map[string]HistogramValue `json:"histogramValue,omitempty"`
+	HistogramValue      map[string]histogramValue `json:"histogramValue,omitempty"`
 }
 
-type HistogramValue struct {
+type histogramValue struct {
 	BucketCount float64 `json:"bucketCount,omitempty"`
 	BucketStart string  `json:"bucketStart,omitempty"`
 	BucketEnd   string  `json:"bucketEnd,omitempty"`
@@ -86,92 +86,92 @@ type MetaData struct {
 	PlatformArchitecture string `json:"platformArchitecture,omitempty"`
 }
 
-type GPUMetrics struct {
+type gpuMetrics struct {
 	CumulativeGPUTime string `json:"cumulativeGPUTime,omitempty"`
 }
 
-type MemoryMetrics struct {
+type memoryMetrics struct {
 	PeakMemoryUsage        string                 `json:"peakMemoryUsage,omitempty"`
-	AverageSuspendedMemory AverageSuspendedMemory `json:"averageSuspendedMemory,omitempty"`
+	AverageSuspendedMemory averageSuspendedMemory `json:"averageSuspendedMemory,omitempty"`
 }
 
-type AverageSuspendedMemory struct {
+type averageSuspendedMemory struct {
 	AverageValue      string  `json:"averageValue,omitempty"`
 	StandardDeviation float64 `json:"standardDeviation,omitempty"`
 	SampleCount       float64 `json:"sampleCount,omitempty"`
 }
 
-type SignpostMetrics struct {
-	SignpostIntervalData SignpostIntervalData `json:"signpostIntervalData,omitempty"`
+type signpostMetrics struct {
+	SignpostIntervalData signpostIntervalData `json:"signpostIntervalData,omitempty"`
 	SignpostCategory     string               `json:"signpostCategory,omitempty"`
 	SignpostName         string               `json:"signpostName,omitempty"`
 	TotalSignpostCount   float64              `json:"totalSignpostCount,omitempty"`
 }
 
-type SignpostIntervalData struct {
-	HistogrammedSignpostDurations   HistogrammedSignpostDurations `json:"histogrammedSignpostDurations,omitempty"`
+type signpostIntervalData struct {
+	HistogrammedSignpostDurations   histogrammedSignpostDurations `json:"histogrammedSignpostDurations,omitempty"`
 	SignpostCumulativeCPUTime       string                        `json:"signpostCumulativeCPUTime,omitempty"`
 	SignpostAverageMemory           string                        `json:"signpostAverageMemory,omitempty"`
 	SignpostCumulativeLogicalWrites string                        `json:"signpostCumulativeLogicalWrites,omitempty"`
 }
 
-type HistogrammedSignpostDurations struct {
+type histogrammedSignpostDurations struct {
 	HistogramNumBuckets float64                   `json:"histogramNumBuckets,omitempty"`
-	HistogramValue      map[string]HistogramValue `json:"histogramValue,omitempty"`
+	HistogramValue      map[string]histogramValue `json:"histogramValue,omitempty"`
 }
 
-type DisplayMetrics struct {
-	AveragePixelLuminance AveragePixelLuminance `json:"averagePixelLuminance,omitempty"`
+type displayMetrics struct {
+	AveragePixelLuminance averagePixelLuminance `json:"averagePixelLuminance,omitempty"`
 }
 
-type AveragePixelLuminance struct {
+type averagePixelLuminance struct {
 	AverageValue      string  `json:"averageValue,omitempty"`
 	StandardDeviation float64 `json:"standardDeviation,omitempty"`
 	SampleCount       float64 `json:"sampleCount,omitempty"`
 }
 
-type CPUMetrics struct {
+type cpuMetrics struct {
 	CumulativeCPUTime string `json:"cumulativeCPUTime,omitempty"`
 }
 
-type NetworkTransferMetrics struct {
+type networkTransferMetrics struct {
 	CumulativeCellularDownload string `json:"cumulativeCellularDownload,omitempty"`
 	CumulativeWifiDownload     string `json:"cumulativeWifiDownload,omitempty"`
 	CumulativeCellularUpload   string `json:"cumulativeCellularUpload,omitempty"`
 	CumulativeWifiUpload       string `json:"cumulativeWifiUpload,omitempty"`
 }
 
-type DiskIOMetrics struct {
+type diskIOMetrics struct {
 	CumulativeLogicalWrites string `json:"cumulativeLogicalWrites,omitempty"`
 }
 
-type ApplicationLaunchMetrics struct {
-	HistogrammedTimeToFirstDrawKey HistogrammedTimeToFirstDrawKey `json:"histogrammedTimeToFirstDrawKey,omitempty"`
-	HistogrammedResumeTime         HistogrammedResumeTime         `json:"histogrammedResumeTime,omitempty"`
+type applicationLaunchMetrics struct {
+	HistogrammedTimeToFirstDrawKey histogrammedTimeToFirstDrawKey `json:"histogrammedTimeToFirstDrawKey,omitempty"`
+	HistogrammedResumeTime         histogrammedResumeTime         `json:"histogrammedResumeTime,omitempty"`
 }
 
-type HistogrammedTimeToFirstDrawKey struct {
+type histogrammedTimeToFirstDrawKey struct {
 	HistogramNumBuckets int64                     `json:"histogramNumBuckets,omitempty"`
-	HistogramValue      map[string]HistogramValue `json:"histogramValue,omitempty"`
+	HistogramValue      map[string]histogramValue `json:"histogramValue,omitempty"`
 }
 
-type HistogrammedResumeTime struct {
+type histogrammedResumeTime struct {
 	HistogramNumBuckets int64                     `json:"histogramNumBuckets,omitempty"`
-	HistogramValue      map[string]HistogramValue `json:"histogramValue,omitempty"`
+	HistogramValue      map[string]histogramValue `json:"histogramValue,omitempty"`
 }
 
-type ApplicationTimeMetrics struct {
+type applicationTimeMetrics struct {
 	CumulativeForegroundTime         string `json:"cumulativeForegroundTime,omitempty"`
 	CumulativeBackgroundTime         string `json:"cumulativeBackgroundTime,omitempty"`
 	CumulativeBackgroundAudioTime    string `json:"cumulativeBackgroundAudioTime,omitempty"`
 	CumulativeBackgroundLocationTime string `json:"cumulativeBackgroundLocationTime,omitempty"`
 }
 
-type ApplicationResponsivenessMetrics struct {
-	HistogrammedAppHangTime HistogrammedAppHangTime `json:"histogrammedAppHangTime,omitempty"`
+type applicationResponsivenessMetrics struct {
+	HistogrammedAppHangTime histogrammedAppHangTime `json:"histogrammedAppHangTime,omitempty"`
 }
 
-type HistogrammedAppHangTime struct {
+type histogrammedAppHangTime struct {
 	HistogramNumBuckets int64                     `json:"histogramNumBuckets,omitempty"`
-	HistogramValue      map[string]HistogramValue `json:"histogramValue,omitempty"`
+	HistogramValue      map[string]histogramValue `json:"histogramValue,omitempty"`
 }
