acc897ed43ee536974d30cf189c8b33188b11c6f
kamaal111 <kamaal.f1@gmail.com>
2021-05-18 19:04:11 +0200

    Improving restrictToHttpMethod

--- src/router/middleware.go
+++ src/router/middleware.go
@@ -12,7 +12,7 @@ import (
 func restrictToHttpMethod(method string, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != method {
-			errorHandler(w, fmt.Sprintf("this request is restricted to a %s method only", method), http.StatusBadRequest)
+			errorHandler(w, fmt.Sprintf("%s does not allow %s. use %s instead", r.URL.String(), r.Method, method), http.StatusMethodNotAllowed)
 			return
 		}
 		next.ServeHTTP(w, r)
